4f523c6ec628297b12f56eb2996e6592d427d16d
Rahul Vijay Singh <singhrahulsingh640@gmail.com>
2023-05-20 15:16:15 +0530

    initial commit with only frontend

---
+++ front_back/index.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@1&display=swap" rel="stylesheet">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css"> 
+</head>
+
+<body>
+  <header>
+    <!-- place navbar here -->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+          <div class="container">
+            <a class="navbar-brand" href="#">Rahul Tourism</a>
+            <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavId">
+                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    
+                </ul>
+                <form class="d-flex my-2 my-lg-0">
+                    <input class="form-control me-sm-2" type="text" placeholder="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+      </div>
+    </nav>
+    </header>
+
+  <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
+      <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
+      <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Third slide"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active">
+        <img src="images/london.jpg" class="w-100 d-block" alt="First slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h3>London</h3>
+          <p>The dream land</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="images/paris.jpg" class="w-100 d-block" alt="Second slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h3>Paris</h3>
+          <p> Dreams of Millions</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="images/swis.jpg" class="w-100 d-block" alt="Third slide">
+        <div class="carousel-caption d-none d-md-block">
+          <h3>Swwitzerland</h3>
+          <p>Get Snowwy</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+      </button>
+  </div>
+
+
+
+    
+ 
+  <main>
+  <section class="my-5">
+    <div class="py-5">
+      <h3 class="text-center">About us</h3>
+    </div> 
+
+
+
+  <!-- container fluid dale kyu ki mere website ke niche scroll bar aa raha tha -->
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+    <img src="images/4.jpg" alt="" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+    <h2 class="display-4">I am Rahul</h2>
+      <p class="py-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus debitis neque fuga, cum ipsum nulla praesentium provident officia tempore modi perferendis dolorem, eius quos et maxime ad sit optio repudiandae?
+      </p> 
+      <a href="about.php" class="btn btn-success">know more</a> 
+
+    </div>
+    
+  </div>
+</div>
+
+ </section>
+
+
+  <section class="my-5">
+    <div class="py-5">
+      <h3 class="text-center">Our Services</h3>
+    </div> 
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/5.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h4 class="card-title">Touch The Shore</h4>
+
+              <a href="#" class="btn btn-primary"> Visit</a>
+            </div>
+            
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/6.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h4 class="card-title">Dive into nature</h4>
+
+              <a href="#" class="btn btn-primary">Visit</a>
+            </div>
+            
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card">
+            <img class="card-img-top" src="images/7.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h4 class="card-title">Feel the hottness</h4>
+              
+              <a href="#" class="btn btn-primary">Visit</a>
+            </div>
+            
+          </div>
+        </div>
+
+        
+
+      </div>
+
+    </div>
+
+</section>
+
+
+
+<section class="my-5">
+    <div class="py-5">
+      <h3 class="text-center">Gallery</h3>
+    </div> 
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+      <img src="images/8.jpg" class="img-fluid pb-3 gallery" alt="image is coiming">    
+      </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/9.jpg" class="img-fluid pb-3 gallery" alt="image is coiming">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+      <img src="images/10.jpg" class="img-fluid pb-3 gallery" alt="image is coiming">    
+      </div>
+        <div class="col-lg-4 col-md-4 col-12">
+      <img src="images/11.jpg" class="img-fluid pb-3 gallery" alt="image is coiming">    
+      </div>
+        <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/12.jpg" class="img-fluid pb-3 gallery" alt="image is coiming"> 
+
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+      <img src="images/13.jpg" class="img-fluid pb-3 gallery" alt="image is coiming">    
+      </div>
+        <div class="col-lg-4 col-md-4 col-12">
+      <img src="images/14.jpg" class="img-fluid pb-3 gallery" alt="image is coiming">    
+      </div>
+        <div class="col-lg-4 col-md-4 col-12">
+      <img src="images/15.jpg" class="img-fluid pb-3 gallery" alt="image is coiming">    
+      </div>
+        <div class="col-lg-4 col-md-4 col-12">
+        <img src="images/16.jpg" class="img-fluid pb-3 gallery" alt="image is coiming">
+        </div>
+      </div>
+</div>
+
+
+</section>
+
+
+ 
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
